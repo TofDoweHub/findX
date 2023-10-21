4a4c05502831676658669cd1ba9f252fbbbd7d09
TofDowe <68544755+TofDoweHub@users.noreply.github.com>
2023-10-21 15:33:27 +0500

    Add files via upload

---
+++ foreach/foreach/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите операцию:");
+                char operation = Convert.ToChar(Console.ReadLine());
+                Console.Write("Введите известное число:");
+                double knownNumber = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите результат:");
+                double result = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(findX(operation, knownNumber, result));
+
+                Console.ReadKey();
+            }
+        }
+        static double findX(char operation, double knownNumber, double result)
+        {
+            switch(operation)
+            {
+                case '/':
+                    return result * knownNumber;
+                case '+':
+                    return result - knownNumber;
+                case '-':
+                    return result + knownNumber;
+                case '*':
+                    return result / knownNumber;
+                default:
+                    Console.WriteLine("Что-то пошло не так... Убедитесь, что не ошиблись при написании!");
+                    return 0;
+            }
+        }
+    }
+}
